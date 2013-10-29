0219218279e10f72bb634077a534fc0323a0f116
Gowri Nambiar <gowri.nambi@gmail.com>
2013-10-29 15:13:37 -0400

    book title done

--- 07_book_titles/book.rb
+++ 07_book_titles/book.rb
@@ -1,14 +1,13 @@
 class Book
-	 
-	
 	def title 
 		@title
 	end
 
 	def title=(m)
-		m.split
-		@title = 
-
+		array = %w[the a and an in of] #create an array without commas and quotations
+		words = m.split 
+		words.each_with_index{|x, index| x.capitalize! unless array.include?(x) && index > 0}
+		@title = words.join " "
 	end
 	
 end
