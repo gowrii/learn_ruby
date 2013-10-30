7bb8c7c1fb0c2848116526cfd45b04092fd68d75
Gowri Nambiar <gowri.nambi@gmail.com>
2013-10-30 00:17:25 -0400

    timer done

--- 08_book_titles/timer.rb
+++ 08_book_titles/timer.rb
@@ -4,40 +4,25 @@ class Timer
 		@seconds
 	end
 	def seconds
-		seconds = 0
+		@seconds = 0
 	end
 def time_string
 	
 hours = @seconds/3600
 minutes = @seconds % 3600 /60
-second = @seconds % 60
+second = @seconds % 60  
 
-"hours.add_zero#{hours}:minutes.add_zero#{minutes}:second.add_zero#{second}"
-
-def add_zero(num)
-	if  num < 10
-		'0'
-	else
-	end
-	
-
-
-	
-
+if second < 10
+"0#{hours}:0#{minutes}:0#{second}"
+elsif second > 10 && minutes > 10
+"0#{hours}:0#{minutes}:#{second}"
+elsif minutes < 10
+"0#{hours}:0#{minutes}:#{second}"
+elsif second < 10 && minutes < 10
+"0#{hours}:0#{minutes}:0#{second}"
+end
 
 end
 end
 
 
-
-
-#	def time_string 
-#		if @seconds == 0
-#		'00:00:00'
-#	else
-#		@seconds %= 60
-#		@minutes %= 60
-#		@minutes = @seconds / 60
-#		@hours = @minutes / 60
-#	end
-#	end
